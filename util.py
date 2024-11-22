524cddad91d2c9abeddad0b8ebe31bb15d8547d7
Tony-Lowe <bigtony001031@icloud.com>
2024-11-22 11:18:14 +0800

    FIx

--- util.py
+++ util.py
@@ -99,7 +99,7 @@ def sep_mask(pos_imgs, sort_radio="↕"):
         contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[1])
     else:
         contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[0])
-    contours = [cnt for cnt in contours if cnt.shape[0] > 10]
+    contours = [cnt for cnt in contours if cnt.shape[0] > 20]
     # poly = []
     # for contour in contours:
     #     rect = cv2.minAreaRect(contour)
